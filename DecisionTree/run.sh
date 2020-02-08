b32657e03d548d2a4b7c8c86f9a0922944432276
SujayTripathy <36439006+champfinal@users.noreply.github.com>
2020-02-08 10:23:51 -0700

    added shell script for cade

---
+++ DecisionTree/run.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+python3 ID3.py 1
+python3 ID3.py 2
+python3 ID3.py 3
+python3 ID3.py 4
+python3 ID3.py 5
+python3 ID3.py 6
+
+python3 ID3Bank.py 1
+python3 ID3Bank.py 2
+python3 ID3Bank.py 3
+python3 ID3Bank.py 4
+python3 ID3Bank.py 5
+python3 ID3Bank.py 6
+python3 ID3Bank.py 7
+python3 ID3Bank.py 8
+python3 ID3Bank.py 9
+python3 ID3Bank.py 10
+python3 ID3Bank.py 11
+python3 ID3Bank.py 12
+python3 ID3Bank.py 13
+python3 ID3Bank.py 14
+python3 ID3Bank.py 15
+python3 ID3Bank.py 16
+
+
